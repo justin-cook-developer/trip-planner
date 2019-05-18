346e027e80589bb2108e44855f3bc850695f7bd5
XtremePineapple <95henryX@gmail.com>
2019-05-18 10:20:00 -0700

    map

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 console.log('Hello from JavaScript');
+
+import mapboxgl from 'mapbox-gl'
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieHRyZW1lcGluZWFwcGxlIiwiYSI6ImNqdnRyeGJ5YzBnYjIzenAxYW1qcmxtN3MifQ.k26FeTLTUTK8xp_rrd8cYw'
+
+const map = new mapboxgl.Map({
+    container: "map",
+    center: [-74.009, 40.705],
+    zoom: 12,
+    style: 'mapbox://styles/mapbox/streets-v10'
+})
